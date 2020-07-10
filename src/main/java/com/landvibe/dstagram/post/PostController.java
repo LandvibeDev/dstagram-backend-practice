a36714ba8c878eff8a7fbc87fe13ff1f105ffb9b
KeonHee <kimkh6554@gmail.com>
2020-07-11 00:15:22 +0900

    2. Service - answer

--- src/main/java/com/landvibe/dstagram/post/PostController.java
+++ src/main/java/com/landvibe/dstagram/post/PostController.java
@@ -5,64 +5,40 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/posts")
 public class PostController {
 
-    private Map<Integer, Post> cachedPosts = new HashMap<>();
+    private PostService postService;
 
-    public PostController() {
+    public PostController(PostService postService) {
+        this.postService = postService;
     }
 
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
     public List<Post> getPosts() {
-        return new ArrayList<>(cachedPosts.values());
+        return this.postService.getPosts();
     }
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public Post createPost(@RequestBody Post post) {
-        LocalDateTime createdTime = LocalDateTime.now();
-        post.setCreated(createdTime);
-        post.setUpdated(createdTime);
-
-        if (cachedPosts.containsKey(post.getId())) {
-            throw new RuntimeException("This post already exists: " + post.getId());
-        } else {
-            cachedPosts.put(post.getId(), post);
-            return post;
-        }
+        return this.postService.createPost(post);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Post updatePost(@PathVariable int id, @RequestBody Post post) {
-        LocalDateTime updatedTime = LocalDateTime.now();
-        post.setUpdated(updatedTime);
-        post.setId(id);
-
-        if (cachedPosts.containsKey(post.getId())) {
-            return cachedPosts.put(id, post);
-        } else {
-            throw new RuntimeException("Not found post: " + id);
-        }
+        return this.postService.updatePost(id, post);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePost(@PathVariable int id) {
-        if (cachedPosts.containsKey(id)) {
-            cachedPosts.remove(id);
-        } else {
-            throw new RuntimeException("Not found post: " + id);
-        }
+        this.postService.deletePost(id);
     }
 }
--- src/main/java/com/landvibe/dstagram/post/PostServiceImpl.java
+++ src/main/java/com/landvibe/dstagram/post/PostServiceImpl.java
@@ -3,32 +3,57 @@ package com.landvibe.dstagram.post;
 import com.landvibe.dstagram.post.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PostServiceImpl implements PostService {
 
+    private Map<Integer, Post> cachedPosts = new HashMap<>();
 
     public PostServiceImpl() {
     }
 
-    @Override
     public List<Post> getPosts() {
-        return null;
+        return new ArrayList<>(cachedPosts.values());
     }
 
     @Override
     public Post createPost(Post post) {
-        return null;
+        LocalDateTime createdTime = LocalDateTime.now();
+        post.setCreated(createdTime);
+        post.setUpdated(createdTime);
+
+        if (cachedPosts.containsKey(post.getId())) {
+            throw new RuntimeException("This post already exists: " + post.getId());
+        } else {
+            cachedPosts.put(post.getId(), post);
+            return post;
+        }
     }
 
     @Override
     public Post updatePost(int id, Post post) {
-        return null;
+        LocalDateTime updatedTime = LocalDateTime.now();
+        post.setUpdated(updatedTime);
+        post.setId(id);
+
+        if (cachedPosts.containsKey(post.getId())) {
+            return cachedPosts.put(id, post);
+        } else {
+            throw new RuntimeException("Not found post: " + id);
+        }
     }
 
     @Override
     public void deletePost(int id) {
-
+        if (cachedPosts.containsKey(id)) {
+            cachedPosts.remove(id);
+        } else {
+            throw new RuntimeException("Not found post: " + id);
+        }
     }
 }
