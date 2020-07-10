649194a1690626b22211e74b8f67140169a4864b
KeonHee <kimkh6554@gmail.com>
2020-07-11 00:08:21 +0900

    1. API - answer

--- src/main/java/com/landvibe/dstagram/post/PostController.java
+++ src/main/java/com/landvibe/dstagram/post/PostController.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,23 +24,45 @@ public class PostController {
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
     public List<Post> getPosts() {
-        return null;
+        return new ArrayList<>(cachedPosts.values());
     }
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public Post createPost(@RequestBody Post post) {
-        return null;
+        LocalDateTime createdTime = LocalDateTime.now();
+        post.setCreated(createdTime);
+        post.setUpdated(createdTime);
+
+        if (cachedPosts.containsKey(post.getId())) {
+            throw new RuntimeException("This post already exists: " + post.getId());
+        } else {
+            cachedPosts.put(post.getId(), post);
+            return post;
+        }
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Post updatePost(@PathVariable int id, @RequestBody Post post) {
-        return null;
+        LocalDateTime updatedTime = LocalDateTime.now();
+        post.setUpdated(updatedTime);
+        post.setId(id);
+
+        if (cachedPosts.containsKey(post.getId())) {
+            return cachedPosts.put(id, post);
+        } else {
+            throw new RuntimeException("Not found post: " + id);
+        }
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePost(@PathVariable int id) {
+        if (cachedPosts.containsKey(id)) {
+            cachedPosts.remove(id);
+        } else {
+            throw new RuntimeException("Not found post: " + id);
+        }
     }
 }
